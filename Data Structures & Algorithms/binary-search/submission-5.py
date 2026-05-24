29d386a40b8bbe1ef6400c912d5a7eca3fdd1876
Raghu narayana <raghupolisetty5@gmail.com>
2026-05-24 22:39:54 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self,nums,target):
+        left=0
+        right=len(nums)
+        while left<right:
+            mid=(left+right)//2
+            if target<nums[mid]:
+                right-=1
+            elif target>nums[mid]:
+                left+=1
+
+            else:
+                return mid
+        return -1
+
+
+
+
+        
