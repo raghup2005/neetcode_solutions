fb138e30741604ae0fb68dae79857c0a2b980341
Raghu narayana <raghupolisetty5@gmail.com>
2026-07-04 14:52:14 +0530

    Add: binary-search - submission-30

---
+++ Data Structures & Algorithms/binary-search/submission-30.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self,nums,target):
+        left=0
+        right=len(nums)-1
+    
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]>target:
+                right-=1
+            elif nums[mid]<target:
+                left+=1
+            else:
+                return mid
+        return -1
+        
